21ef54a196c80fa1c4f020bb4819e4b27131fe54
fundmarkhua <fundmark@163.com>
2016-10-15 16:50:43 +0800

    建立数据表 及其实体类

---
+++ app/src/main/java/com/example/hua/coolweather/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,53 @@
+package com.example.hua.coolweather.db;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by fundmarkhua on 2016/10/15.
+ * 主要用于项目中 Province(省份), City(城市), County(区县) 三个数据表的创建
+ */
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+    /**
+     * Province表建表语句
+     */
+    public static final String CREATE_PROVINCE = "create table Province ("
+            + "id integer primary key autoincrement, "
+            + "province_name text, "
+            + "province_code text)";
+    /**
+     * City表建表语句
+     */
+    public static final String CREATE_CITY = "create table City ("
+            + "id integer primary key autoincrement, "
+            + "city_name text, "
+            + "city_code text, "
+            + "province_id integer)";
+    /**
+     * County表建表语句
+     */
+    public static final String CREATE_COUNTY = "create table County ("
+            + "id integer primary key autoincrement, "
+            + "county_name text, "
+            + "county_code text, "
+            + "city_id integer)";
+
+    public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE); //创建Province省份表
+        db.execSQL(CREATE_CITY);     //创建City城市表
+        db.execSQL(CREATE_COUNTY);   //创建Country区县表
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/hua/coolweather/model/Province.java
@@ -0,0 +1,36 @@
+package com.example.hua.coolweather.model;
+
+/**
+ * Created by fundmarkhua on 2016/10/15.
+ * 数据表Province 的数据实体类
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
